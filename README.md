f367a206e31764c5bb6dfa11f2f79466cf206a45
unbreakablery <unbreakablery@gmail.com>
2021-07-02 01:27:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# OOP & CRUD in PHP [AddressBook]
+
+This is example for Object Oriented Programming and CRUD in PHP
+
+- PHP v5.x, 7,x, and 8.x
+- PDO for MySQL
+- Bootstrap 4 & JQuery
---
+++ error.php
@@ -0,0 +1,14 @@
+<?php include "inc/header.php"; ?>
+
+<section id="content">
+    <div class="container">
+        <div class="col-12 mt-4 mb-4">
+            <h2 class="font-weight-bolder text-danger">Sorry, Operation Error!</h2>
+        </div>
+        <div class="col-12">
+            <a href="./index.php" class="btn btn-secondary">Back</a>
+        </div>
+    </div>
+</section>
+
+<?php include "inc/footer.php"; ?>
